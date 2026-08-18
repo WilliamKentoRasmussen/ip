030bff9da1d4d8ffe81ccaca56917751a2ca2fec
WilliamKentoRasmussen <s245310@dtu.dk>
2026-08-18 09:54:57 +0800

    Add Kento instead of duke

---
+++ src/main/java/Kento.java
@@ -0,0 +1,10 @@
+public class Kento {
+    public static void main(String[] args) {
+        String banner = " _  __          _  _____  \n"
+                + "| |/ /___ _ __ | |/ / _ \\\n"
+                + "| ' // _ \\ '_ \\| ' / (_) |\n"
+                + "| . \\  __/ | | | . \\__, |\n"
+                + "|_|\\_\\___|_| |_|_|\\_\\__/\n";
+        System.out.println(banner);
+    }
+}
