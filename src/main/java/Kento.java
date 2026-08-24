3a9f1e348fe94aae0ffc804e28b11ae0aba38850
WilliamKentoRasmussen <s245310@dtu.dk>
2026-08-24 18:32:36 +0800

    Add echo

--- src/main/java/Kento.java
+++ src/main/java/Kento.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Kento {
+
+    private static boolean running = true;
+
     public static void main(String[] args) {
+
         String banner = " _  __         ____  ___      \n"
                 + "| |/ /___ _ __|_  _|/   \\\n"
                 + "| ' // _ \\ '_ \\| | / (_) |\n"
@@ -10,13 +16,51 @@ public class Kento {
                 ____________________________________________________________
                 What is the service you are willing to pay the most for?
                 ____________________________________________________________
-                Bye. Hope to see you again soon!
-                ____________________________________________________________
-
                 """;
 
         System.out.println("Welcome to");
         System.out.println(banner);
         System.out.println(greeting);
+
+        Scanner in = new Scanner(System.in);
+
+        String cmd;
+        while (running) {
+            cmd = in.nextLine();
+
+            switch (cmd.toLowerCase()) {
+                case ("bye"):
+                    System.out.print("""
+                            ____________________________________________________________
+                             Bye, don't come back without more money!
+                            ____________________________________________________________
+                                            """);
+                    running = false;
+                    break;
+
+                default:
+                    System.out.print(String.format("""
+
+                            ____________________________________________________________
+                            %s
+                            ____________________________________________________________
+                                            """, cmd));
+                    break;
+            }
+        }
     }
 }
+
+/*
+ * Level-1, Level-2, Level-3, A-CodingStandard
+ * 
+ * Bye.Hope to
+ * see you
+ * again soon!
+ * switch (cmd){
+ * switch("list"):
+ * System.out.print("");
+ * break;
+ * }
+ * ____________________________________________________________
+ */
